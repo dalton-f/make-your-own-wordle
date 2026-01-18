168c1c36bc7678d13b0a3cf120bcc0d444a68af3
Dalton <daltonfiggdavies@gmail.com>
2026-01-18 09:24:20 +0000

    Implement main Wordle game logic

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,99 @@ const board = document.getElementById("board");
 const rows = 6; // Number of attempts/guesses the user has
 const cols = 5; // Length of the word
 
-for (let i = 0; i < rows; i++) {
-  const row = document.createElement("div");
-  row.className = "row";
+let currentRow = 0;
+let currentCol = 0;
+let currentGuess = "";
 
-  for (let j = 0; j < cols; j++) {
-    const tile = document.createElement("div");
-    tile.className = "tile";
-    tile.dataset.state = "empty";
+let correctWord = "FIERY";
 
-    row.appendChild(tile);
+const initializeBoard = () => {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    for (let j = 0; j < cols; j++) {
+      const tile = document.createElement("div");
+      tile.className = "tile";
+      tile.dataset.state = "empty";
+      tile.dataset.id = `${i}${j}`;
+
+      row.appendChild(tile);
+    }
+
+    fragment.appendChild(row);
   }
 
-  board.appendChild(row);
-}
+  board.appendChild(fragment);
+};
+
+initializeBoard();
+
+const deleteLetter = () => {
+  if (currentCol <= 0) return;
+
+  // Move to the previous tile
+  currentCol -= 1;
+
+  const targetTile = fetchTargetTile();
+  targetTile.innerHTML = "";
+
+  // Internally track a version of the current guess
+  currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+};
+
+const addLetter = (event) => {
+  if (currentCol >= cols) return;
+
+  // Add the input letter to the correct tile
+  const targetTile = fetchTargetTile();
+  targetTile.innerHTML = event.key;
+
+  // Move to the next tile
+  currentCol += 1;
+
+  // Internally track a version of the current guess
+  currentGuess += event.key.toUpperCase();
+};
+
+const fetchTargetTile = () => {
+  const targetTile = document.querySelector(
+    `[data-id="${currentRow}${currentCol}"]`,
+  );
+
+  return targetTile;
+};
+
+const submitGuess = () => {
+  if (currentGuess.length < cols) return;
+
+  // Compares the current guess to the correct word
+  for (let i = 0; i < cols; i++) {
+    const letter = currentGuess[i];
+    // Ensures we keep track of the correct tile to change the state of within the current row
+    const tile = document.querySelector(`[data-id="${currentRow}${i}"]`);
+
+    // Update state accordingly
+    let state = "absent";
+
+    if (correctWord[i] === letter) state = "correct";
+    else if (correctWord.includes(letter)) state = "present";
+
+    tile.dataset.state = state;
+  }
+
+  // Update row and column values to allow for the next guess
+  currentRow++;
+  currentCol = 0;
+  currentGuess = "";
+};
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Backspace") deleteLetter();
+
+  if (event.key === "Enter") submitGuess();
+
+  if (event.key.match(/[a-z]/i) && event.key.length === 1) addLetter(event);
+});
--- static/js/app.js
+++ static/js/app.js
@@ -24,17 +24,80 @@ var board = document.getElementById("board");
 var rows = 6; // Number of attempts/guesses the user has
 var cols = 5; // Length of the word
 
-for (var i = 0; i < rows; i++) {
-  var row = document.createElement("div");
-  row.className = "row";
-  for (var j = 0; j < cols; j++) {
-    var tile = document.createElement("div");
-    tile.className = "tile";
-    tile.dataset.state = "empty";
-    row.appendChild(tile);
+var currentRow = 0;
+var currentCol = 0;
+var currentGuess = "";
+var correctWord = "FIERY";
+var initializeBoard = function initializeBoard() {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < rows; i++) {
+    var row = document.createElement("div");
+    row.className = "row";
+    for (var j = 0; j < cols; j++) {
+      var tile = document.createElement("div");
+      tile.className = "tile";
+      tile.dataset.state = "empty";
+      tile.dataset.id = "".concat(i).concat(j);
+      row.appendChild(tile);
+    }
+    fragment.appendChild(row);
   }
-  board.appendChild(row);
-}
+  board.appendChild(fragment);
+};
+initializeBoard();
+var deleteLetter = function deleteLetter() {
+  if (currentCol <= 0) return;
+
+  // Move to the previous tile
+  currentCol -= 1;
+  var targetTile = fetchTargetTile();
+  targetTile.innerHTML = "";
+
+  // Internally track a version of the current guess
+  currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+};
+var addLetter = function addLetter(event) {
+  if (currentCol >= cols) return;
+
+  // Add the input letter to the correct tile
+  var targetTile = fetchTargetTile();
+  targetTile.innerHTML = event.key;
+
+  // Move to the next tile
+  currentCol += 1;
+
+  // Internally track a version of the current guess
+  currentGuess += event.key.toUpperCase();
+};
+var fetchTargetTile = function fetchTargetTile() {
+  var targetTile = document.querySelector("[data-id=\"".concat(currentRow).concat(currentCol, "\"]"));
+  return targetTile;
+};
+var submitGuess = function submitGuess() {
+  if (currentGuess.length < cols) return;
+
+  // Compares the current guess to the correct word
+  for (var i = 0; i < cols; i++) {
+    var letter = currentGuess[i];
+    // Ensures we keep track of the correct tile to change the state of within the current row
+    var tile = document.querySelector("[data-id=\"".concat(currentRow).concat(i, "\"]"));
+
+    // Update state accordingly
+    var state = "absent";
+    if (correctWord[i] === letter) state = "correct";else if (correctWord.includes(letter)) state = "present";
+    tile.dataset.state = state;
+  }
+
+  // Update row and column values to allow for the next guess
+  currentRow++;
+  currentCol = 0;
+  currentGuess = "";
+};
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Backspace") deleteLetter();
+  if (event.key === "Enter") submitGuess();
+  if (event.key.match(/[a-z]/i) && event.key.length === 1) addLetter(event);
+});
 
 /***/ }
 
